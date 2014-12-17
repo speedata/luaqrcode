d1f41390cb412b0051058c8ec7def4882eb4cea5
Patrick Gundlach <gundlach@speedata.de>
2014-12-17 19:44:27 +0100

    Update to new locco version
    No code change to luaqrencode

--- locco/locco.lua
+++ locco/locco.lua
@@ -1,4 +1,4 @@
-#!/usr/bin/env sdtexlua
+#!/usr/bin/env lua
 -- __Locco__ is a Lua port of [Docco](http://jashkenas.github.com/docco/),
 -- the quick-and-dirty, hundred-line-long, literate-programming-style
 -- documentation generator. It produces HTML that displays your comments
@@ -8,10 +8,14 @@
 -- This page is the result of running Locco against its own source file:
 --     locco.lua locco.lua
 --
--- For its syntax highlighting Locco relies on the help of David Manura's
--- [Lua Balanced](http://lua-users.org/wiki/LuaBalanced) to split
--- up the code. As a markdown engine it ships with Niklas Frykholm's
--- [markdown.lua](http://www.frykholm.se/files/markdown.lua). Otherwise there
+-- For its syntax highlighting Locco relies on the help of
+-- [David Manura](http://lua-users.org/wiki/DavidManura)'s
+-- [Lua Balanced](https://github.com/davidm/lua-balanced) to split
+-- up the code. As a markdown engine it ships with
+-- [Niklas Frykholm](http://www.frykholm.se/)'s
+-- [markdown.lua](http://www.frykholm.se/files/markdown.lua) in the [Lua 5.2
+-- compatible version](https://github.com/speedata/luamarkdown) from
+-- [Patrick Gundlach](https://github.com/pgundlach). Otherwise there
 -- are no external dependencies.
 --
 -- The generated HTML documentation for the given source files is saved
@@ -19,32 +23,127 @@
 -- the command-line:
 --     locco.lua project/*.lua
 --
--- Locco is monolingual, but there are also
--- [Docco](http://jashkenas.github.com/docco/),
--- [Pycco](http://fitzgen.github.com/pycco/),
--- [Rocco](http://rtomayko.github.com/rocco/>)
--- and
--- [Shocco](http://rtomayko.github.com/shocco/)
--- written in and with support for other languages.<br>
--- The source for Locco is available on
--- [GitHub](https://github.com/rgieseke/locco), and released under the MIT
+-- Locco is monolingual, but there are many projects written in
+-- and with support for other languages, see the
+-- [Docco](http://jashkenas.github.com/docco/) page for a list.<br>
+-- The [source for Locco](https://github.com/rgieseke/locco) is available on
+-- GitHub, and released under the MIT
 -- license.
 
--- ### Main Documentation Generation Functions
+-- ### Setup & Helpers
 
--- Generate the documentation for a source file by reading it in,
--- splitting it up into comment/code sections, highlighting and merging
--- them into an HTML template.<br>
--- Parameters:<br>
--- _source_: The source file to process.<br>
--- _path_: Path of the source file.<br>
--- _filename_: The filename of the source file.<br>
--- _jump\_to_: A HTML chunk with links to other documentation files.
-function generate_documentation(source, path, filename, jump_to)
-  local sections = parse(source)
-  local sections = highlight(sections)
-  generate_html(source, path, filename, sections, jump_to)
+-- Add script path to package path to find submodules.
+local script_path = arg[0]:match('(.+)/.+')
+package.path = table.concat({
+  script_path..'/?.lua',
+  package.path
+}, ';')
+
+-- Load markdown.lua.
+local md = require 'markdown'
+-- Load Lua Balanced.
+local lb = require 'luabalanced'
+-- Load HTML templates.
+local template = require 'template'
+
+-- Ensure the `docs` directory exists and return the _path_ of the source file.<br>
+-- Parameter:<br>
+-- _source_: The source file for which documentation is generated.<br>
+local function ensure_directory(source)
+  local path = source:match('(.+)/.+$')
+  if not path then path = '.' end
+  os.execute('mkdir -p '..path..'/ghpages/docs')
+  return path
 end
+
+-- Insert HTML entities in a string.<br>
+-- Parameter:<br>
+-- _s_: String to escape.<br>
+local function escape(s)
+  s = s:gsub('&', '&amp;')
+  s = s:gsub('<', '&lt;')
+  s = s:gsub('>', '&gt;')
+  s = s:gsub('%%', '&#37;')
+  return s
+end
+
+--local function replace_percent(s)
+--  s = s:gsub('%%', '%%%%')
+--  return s
+--end
+
+-- Define the Lua keywords, built-in functions and operators that should
+-- be highlighted.
+local keywords = { 'break', 'do', 'else', 'elseif', 'end', 'false', 'for',
+                   'function', 'if', 'in', 'local', 'nil', 'repeat', 'return',
+                   'then', 'true', 'until', 'while' }
+local functions = { 'assert', 'collectgarbage', 'dofile', 'error', 'getfenv',
+                    'getmetatable', 'ipairs', 'load', 'loadfile', 'loadstring',
+                    'module', 'next', 'pairs', 'pcall', 'print', 'rawequal',
+                    'rawget', 'rawset', 'require', 'setfenv', 'setmetatable',
+                    'tonumber', 'tostring', 'type', 'unpack', 'xpcall' }
+local operators = { 'and', 'not', 'or' }
+
+-- Wrap an item from a list of Lua keywords in a span template or return the
+-- unchanged item.<br>
+-- Parameters:<br>
+-- _item_: An item of a code snippet.<br>
+-- _item\_list_: List of keywords or functions.<br>
+-- _span\_class_: Style sheet class.<br>
+local function wrap_in_span(item, item_list, span_class)
+  for i=1, #item_list do
+    if item_list[i] == item then
+      item = '<span class="'..span_class..'">'..item..'</span>'
+      break
+    end
+  end
+  return item
+end
+
+-- Quick and dirty source code highlighting. A chunk of code is split into
+-- comments (at the end of a line), strings and code using the
+-- [Lua Balanced](https://github.com/davidm/lua-balanced/blob/master/luabalanced.lua)
+-- module. The code is then split again and matched against lists
+-- of Lua keywords, functions or operators. All Lua items are wrapped into
+-- a span having one of the classes defined in the Locco style sheet.<br>
+-- Parameter:<br>
+-- _code_: Chunk of code to highlight.<br>
+local function highlight_lua(code)
+  local out = lb.gsub(code,
+    function(u, s)
+      local sout
+      if u == 'c' then -- Comments.
+        sout = '<span class="c">'..escape(s)..'</span>'
+      elseif u == 's' then -- Strings.
+        sout = '<span class="s">'..escape(s)..'</span>'
+      elseif u == 'e' then -- Code.
+        s = escape(s)
+        -- First highlight function names.
+        s = s:gsub('function ([%w_:%.]+)', 'function <span class="nf">%1</span>')
+        -- There might be a non-keyword at the beginning of the snippet.
+        sout = s:match('^(%A+)') or ''
+        -- Iterate through Lua items and try to wrap operators,
+        -- keywords and built-in functions in span elements.
+        -- If nothing was highlighted go to the next category.
+        for item, sep in s:gmatch('([%a_]+)(%A+)') do
+          local span, n = wrap_in_span(item, operators, 'o')
+          if span == item then
+            span, n = wrap_in_span(item, keywords, 'k')
+          end
+          if span == item then
+            span, n = wrap_in_span(item, functions, 'nt')
+          end
+          sout = sout..span..sep
+        end
+      end
+      return sout
+    end)
+    out = '<div class="highlight"><pre>'..out..'</pre></div>'
+  return out
+end
+
+
+-- ### Main Documentation Generation Functions
 
 -- Given a string of source code, parse out each comment and the code that
 -- follows it, and create an individual section for it. Sections take the form:
@@ -58,7 +157,7 @@ end
 --
 -- Parameter:<br>
 -- _source_: The source file to process.<br>
-function parse(source)
+local function parse(source)
   local sections = {}
   local has_code = false
   local docs_text, code_text = '', ''
@@ -90,9 +189,9 @@ end
 -- table.<br>
 -- Parameter:<br>
 -- _sections_: A table with split sections.<br>
-function highlight(sections)
+local function highlight(sections)
   for i=1, #sections do
-    sections[i]['docs_html'] = markdown(sections[i]['docs_text'])
+    sections[i]['docs_html'] = md.markdown(sections[i]['docs_text'])
     sections[i]['code_html'] = highlight_lua(sections[i]['code_text'])
   end
   return sections
@@ -106,8 +205,8 @@ end
 -- _filename_: The filename of the source file.<br>
 -- _sections_: A table with the original sections and rendered as HTML.<br>
 -- _jump\_to_: A HTML chunk with links to other documentation files.
-function generate_html(source, path, filename, sections, jump_to)
-  f, err = io.open(path..'/ghpages/docs/'..filename:gsub('lua$', 'html'), 'wb')
+local function generate_html(source, path, filename, sections, jump_to)
+  local f, err = io.open(path..'/ghpages/docs/'..filename:gsub('lua$', 'html'), 'wb')
   if err then print(err) end
   local h = template.header:gsub('%%title%%', source)
   h = h:gsub('%%jump%%', jump_to)
@@ -122,110 +221,20 @@ function generate_html(source, path, filename, sections, jump_to)
   f:close()
 end
 
--- ### Helpers & Setup
-
--- We need the script location to add the script's directory to the package
--- path and to copy the style sheet from.
-script_path = arg[0]:match('(.+)/.+')
-package.path = table.concat({
-  script_path..'/?.lua',
-  package.path
-}, ';');
--- Load markdown.lua. It creates a global `markdown` function
-require 'markdown'
--- Load Lua Balanced.
-lb = require('luabalanced')
--- Load HTML templates.
-require 'template'
-
--- Ensure the `docs` directory exists and return the _path_ of the source file.<br>
--- Parameter:<br>
--- _source_: The source file for which documentation is generated.<br>
-function ensure_directory(source)
-  local path = source:match('(.+)/.+$')
-  if not path then path = '.' end
-  os.execute('mkdir -p '..path..'/ghpages/docs')
-  return path
-end
-
--- Insert HTML entities in a string.<br>
--- Parameter:<br>
--- _s_: String to escape.<br>
-function escape(s)
-  s = s:gsub('<', '&lt;')
-  s = s:gsub('>', '&gt;')
-  s = s:gsub('%%', '&#37;')
-  return s
-end
-
--- Define the Lua keywords, built-in functions and operators that should
--- be highlighted.
-local keywords = { 'break', 'do', 'else', 'elseif', 'end', 'false', 'for',
-                   'function', 'if', 'in', 'local', 'nil', 'repeat', 'return',
-                   'then', 'true', 'until', 'while' }
-local functions = { 'assert', 'collectgarbage', 'dofile', 'error', 'getfenv',
-                    'getmetatable', 'ipairs', 'load', 'loadfile', 'loadstring',
-                    'module', 'next', 'pairs', 'pcall', 'print', 'rawequal',
-                    'rawget', 'rawset', 'require', 'setfenv', 'setmetatable',
-                    'tonumber', 'tostring', 'type', 'unpack', 'xpcall' }
-local operators = { 'and', 'not', 'or' }
-
--- Wrap an item from a list of Lua keywords in a span template or return the
--- unchanged item.<br>
+-- Generate the documentation for a source file by reading it in,
+-- splitting it up into comment/code sections, highlighting and merging
+-- them into an HTML template.<br>
 -- Parameters:<br>
--- _item_: An item of a code snippet.<br>
--- _item\_list_: List of keywords or functions.<br>
--- _span\_class_: Style sheet class.<br>
-function wrap_in_span(item, item_list, span_class)
-  for i=1, #item_list do
-    if item_list[i] == item then
-      item = '<span class="'..span_class..'">'..item..'</span>'
-      break
-    end
-  end
-  return item
+-- _source_: The source file to process.<br>
+-- _path_: Path of the source file.<br>
+-- _filename_: The filename of the source file.<br>
+-- _jump\_to_: A HTML chunk with links to other documentation files.
+local function generate_documentation(source, path, filename, jump_to)
+  local sections = parse(source)
+  local sections = highlight(sections)
+  generate_html(source, path, filename, sections, jump_to)
 end
 
--- Quick and dirty source code highlighting. A chunk of code is split into
--- comments (at the end of a line), strings and code using the
--- [Lua Balanced](https://github.com/davidm/lua-balanced/blob/master/luabalanced.lua)
--- module. The code is then split again and matched against lists
--- of Lua keywords, functions or operators. All Lua items are wrapped into
--- a span having one of the classes defined in the Locco style sheet.<br>
--- Parameter:<br>
--- _code_: Chunk of code to highlight.<br>
-function highlight_lua(code)
-    local out = lb.gsub(code,
-      function(u, s)
-        local sout
-        if u == 'c' then -- Comments.
-          sout = '<span class="c">'..escape(s)..'</span>'
-        elseif u == 's' then -- Strings.
-          sout = '<span class="s">'..escape(s)..'</span>'
-        elseif u == 'e' then -- Code.
-          -- First highlight function names.
-          s = s:gsub('function ([%w_:%.]+)', 'function <span class="nf">%1</span>')
-          -- There might be a non-keyword at the beginning of the snippet.
-          sout = s:match('^(%A+)') or ''
-          -- Iterate through Lua items and try to wrap operators,
-          -- keywords and built-in functions in span elements.
-          -- If nothing was highlighted go to the next category.
-          for item, sep in s:gmatch('([%a_]+)(%A+)') do
-            local span, n = wrap_in_span(item, operators, 'o')
-            if span == item then
-              span, n = wrap_in_span(item, keywords, 'k')
-            end
-            if span == item then
-              span, n = wrap_in_span(item, functions, 'nt')
-            end
-            sout = sout..span..sep
-          end
-        end
-        return sout
-      end)
-      out = '<div class="highlight"><pre>'..out..'</pre></div>'
-    return out
-  end
 
 -- Run the script.
 
@@ -244,11 +253,14 @@ if #arg > 1 then
 end
 
 -- Make sure the output directory exists, generate the HTML files for each
--- source file, print what's happening and copy the style sheet.
+-- source file, print what's happening and write the style sheet.
 local path = ensure_directory(arg[1])
 for i=1, #arg do
   local filename = arg[i]:match('.+/(.+)$') or arg[i]
   generate_documentation(arg[i], path, filename, jump_to)
   print(arg[i]..' --> '..path..'/ghpages/docs/'..filename:gsub('lua$', 'html'))
 end
-os.execute('cp '..script_path..'/locco.css '..path..'/ghpages/docs')
+local f, err = io.open(path..'/'..'ghpages/docs/locco.css', 'wb')
+if err then print(err) end
+f:write(template.css)
+f:close()
--- locco/luabalanced.lua
+++ locco/luabalanced.lua
@@ -1,28 +1,160 @@
--- luabalanced.lua
--- Extracted delimited Lua sequences from strings.[1]
--- Inspired by Damian Conway's Text::Balanced[2] in Perl.
---
---   [1] http://lua-users.org/wiki/LuaBalanced
---   [2] http://search.cpan.org/dist/Text-Balanced/lib/Text/Balanced.pm
---
--- (c) 2008, David Manura, Licensed under the same terms as Lua (MIT license).
---
+--[==[
 
-local M = {}
+LUA MODULE
+ 
+  luabalanced v$(_VERSION) - Functions for matching delimited snippets of Lua code in a string
+  
+SYNOPSIS
+
+  local LB = require "luabalanced"
+  -- Extract Lua expression starting at position 4.
+  print(LB.match_expression("if x^2 + x > 5 then print(x) end", 4))
+  --> x^2 + x > 5     16
+  -- Extract Lua string starting at (default) position 1.
+  print(LB.match_string([["test\"123" .. "more"]]))
+  --> "test\"123"     12
+  -- Break Lua code into code types.
+  LB.gsub([[
+    local x = 1  -- test
+    print("x=", x)
+  ]], function(u, s)
+    print(u .. '[' .. s .. ']')
+  end)
+  --[[output:
+  e[  local x = 1  ]
+  c[-- test
+  ]
+  e[  print(]
+  s["x="]
+  e[, x)
+  ]
+  ]]
+
+DESCRIPTION
+  
+  This module can, for example, match a Lua string, Lua comment, or Lua
+  expression. It is useful in particular for source filters or parsing
+  Lua snippets embedded in another language. It is inspired by Damian Conway's
+  Text::Balanced [1] in Perl. The unique feature of this implementation
+  is that that it does not rigorously lex and parse the Lua grammar.
+  It doesn't need to. It assumes during the parse that the Lua code is
+  syntactically correct (which can be verified later using loadstring).
+  By assuming this, extraction of delimited sequences is significantly
+  simplified yet can still be robust, and it also supports supersets
+  of the Lua grammar. The code, which is written entirely in Lua,
+  is just under 200 lines of Lua code (compare to Yueliang used in
+  MetaLua, where the lexer alone is a few hundred lines).
+
+API
+
+  LB.match_string(s, pos) --> string, posnew
+  
+    Match Lua string in string <s> starting at position `pos`.
+    Returns `string`, `posnew`, where `string` is the matched
+    string (or nil on no match) and `posnew` is the character
+    following the match (or `pos` on no match).
+    Supports all Lua string syntax: "...", '...', [[...]], [=[...]=], etc.
+
+  LB.match_bracketed(s, pos) --> string, posnew
+
+    Match bracketed Lua expression, e.g. "(...)", "{...}", "[...]", "[[...]]",
+    [=[...]=], etc.
+    Function interface is similar to `match_string`.
+    
+  LB.match_comment(s, pos) --> string, posnew
+  
+    Match Lua comment, e.g. "--...\n", "--[[...]]", "--[=[...]=]", etc.
+    Function interface is similar to `match_string`.
+
+  LB.match_expression(s, pos) --> string, posnew
+    
+    Match Lua expression, e.g. "a + b * c[e]".
+    Function interface is similar to match_string.
+
+  LB.match_namelist(s, pos) --> array, posnew
+  
+    Match name list (zero or more names).  E.g. "a,b,c"
+    Function interface is similar to match_string,
+    but returns array as match.
+
+  M.match_explist(s, pos) --> array, posnew
+    
+    Match expression list (zero or more expressions).  E.g. "a+b,b*c".
+    Function interface is similar to match_string,
+    but returns array as match.
+
+  M.gsub(s, f)
+  
+    Replace snippets of code in Lua code string `s`
+    using replacement function `f(u,sin) --> sout`.
+    `u` is the type of snippet ('c' = comment, 's' = string,
+    'e' = any other code).
+    Snippet is replaced with `sout` (unless `sout` is `nil` or `false`, in
+    which case the original snippet is kept)
+    This is somewhat analogous to `string.gsub`.
+    
+DEPENDENCIES
+
+  None (other than Lua 5.1 or 5.2).
+  
+HOME PAGE
+
+  http://lua-users.org/wiki/LuaBalanced
+  https://github.com/davidm/lua-balanced
+
+DOWNLOAD/INSTALL
+
+  If using LuaRocks:
+    luarocks install lua-balanced
+
+  Otherwise, download <https://github.com/davidm/lua-balanced/zipball/master>
+  and unzip. Alternately, if using git:
+    git clone git://github.com/davidm/lua-balanced.git
+    cd lua-balanced
+  Optionally unpack:
+    ./util.mk
+  or unpack and install in LuaRocks:
+    ./util.mk install 
+  
+REFERENCES
+
+  [1] http://lua-users.org/wiki/LuaBalanced
+  [2] http://search.cpan.org/dist/Text-Balanced/lib/Text/Balanced.pm
+
+ LICENSE
+
+  (c) 2008-2011 David Manura.  Licensed under the same terms as Lua (MIT).
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  (end license)
+
+--]==]---------------------------------------------------------------------
+
+local M = {_TYPE='module', _NAME='luabalanaced', _VERSION='0.1.1.20120323'}
 
 local assert = assert
-local table_concat = table.concat
 
 -- map opening brace <-> closing brace.
 local ends = { ['('] = ')', ['{'] = '}', ['['] = ']' }
 local begins = {}; for k,v in pairs(ends) do begins[v] = k end
 
 
--- Match Lua string in string <s> starting at position <pos>.
--- Returns <string>, <posnew>, where <string> is the matched
--- string (or nil on no match) and <posnew> is the character
--- following the match (or <pos> on no match).
--- Supports all Lua string syntax: "...", '...', [[...]], [=[...]=], etc.
 local function match_string(s, pos)
   pos = pos or 1
   local posa = pos
@@ -53,9 +185,6 @@ end
 M.match_string = match_string
 
 
--- Match bracketed Lua expression, e.g. "(...)", "{...}", "[...]", "[[...]]",
--- [=[...]=], etc.
--- Function interface is similar to match_string.
 local function match_bracketed(s, pos)
   pos = pos or 1
   local posa = pos
@@ -99,8 +228,6 @@ end
 M.match_bracketed = match_bracketed
 
 
--- Match Lua comment, e.g. "--...\n", "--[[...]]", "--[=[...]=]", etc.
--- Function interface is similar to match_string.
 local function match_comment(s, pos)
   pos = pos or 1
   if s:sub(pos, pos+1) ~= '--' then
@@ -116,8 +243,6 @@ local function match_comment(s, pos)
 end
 
 
--- Match Lua expression, e.g. "a + b * c[e]".
--- Function interface is similar to match_string.
 local wordop = {['and']=true, ['or']=true, ['not']=true}
 local is_compare = {['>']=true, ['<']=true, ['~']=true}
 local function match_expression(s, pos)
@@ -174,9 +299,6 @@ end
 M.match_expression = match_expression
 
 
--- Match name list (zero or more names).  E.g. "a,b,c"
--- Function interface is similar to match_string,
--- but returns array as match.
 local function match_namelist(s, pos)
   pos = pos or 1
   local list = {}
@@ -191,9 +313,6 @@ end
 M.match_namelist = match_namelist
 
 
--- Match expression list (zero or more expressions).  E.g. "a+b,b*c".
--- Function interface is similar to match_string,
--- but returns array as match.
 local function match_explist(s, pos)
   pos = pos or 1
   local list = {}
@@ -211,25 +330,18 @@ end
 M.match_explist = match_explist
 
 
--- Replace snippets of code in Lua code string <s>
--- using replacement function f(u,sin) --> sout.
--- <u> is the type of snippet ('c' = comment, 's' = string,
--- 'e' = any other code).
--- Snippet is replaced with <sout> (unless <sout> is nil or false, in
--- which case the original snippet is kept)
--- This is somewhat analogous to string.gsub .
 local function gsub(s, f)
   local pos = 1
   local posa = 1
-  local sret = ''
+  local ts = {}
   while 1 do
     pos = s:find('[%-\'\"%[]', pos)
     if not pos then break end
     if s:match('^%-%-', pos) then
       local exp = s:sub(posa, pos-1)
-      if #exp > 0 then sret = sret .. (f('e', exp) or exp) end
+      if #exp > 0 then ts[#ts+1] = (f('e', exp) or exp) end
       local comment; comment, pos = match_comment(s, pos)
-      sret = sret .. (f('c', assert(comment)) or comment)
+      ts[#ts+1] = (f('c', assert(comment)) or comment)
       posa = pos
     else
       local posb = s:find('^[\'\"%[]', pos)
@@ -237,8 +349,8 @@ local function gsub(s, f)
       if posb then str, pos = match_string(s, posb) end
       if str then
         local exp = s:sub(posa, posb-1)
-        if #exp > 0 then sret = sret .. (f('e', exp) or exp) end
-        sret = sret .. (f('s', str) or str)
+        if #exp > 0 then ts[#ts+1] = (f('e', exp) or exp) end
+        ts[#ts+1] = (f('s', str) or str)
         posa = pos
       else
         pos = pos + 1
@@ -246,8 +358,8 @@ local function gsub(s, f)
     end
   end
   local exp = s:sub(posa)
-  if #exp > 0 then sret = sret .. (f('e', exp) or exp) end
-  return sret
+  if #exp > 0 then ts[#ts+1] = (f('e', exp) or exp) end
+  return table.concat(ts)
 end
 M.gsub = gsub
 
